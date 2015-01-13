510f35d6708227332cd89fc63d0a13d3f65200eb
Joakim Ribier <joakim.ribier@gmail.com>
2015-01-13 23:17:55 +0100

    Update global CSS

--- app/assets/javascripts/loginController.js
+++ app/assets/javascripts/loginController.js
@@ -2,23 +2,24 @@
 
 var myApp = angular.module('reactiveElasticsearchPlay.controllers',[]);
 myApp.controller('LoginController', ['$scope', '$log', '$http', '$window', '$location', function ($scope, $log, $http, $window, $location) {
-	$scope.loading = false;
-	$scope.validationMessage = '';
-	$scope.credentials = {
-		username: '',
-		password: ''
-	};
+    $scope.loading = false;
+    $scope.validationMessage = '';
+    $scope.credentials = {
+        username: '',
+        password: ''
+    };
 	
-	$scope.login = function (credentials) {
-		$scope.loading = true;
-		$http.post($window.hostName + '/page/login/authentication', {credentials: credentials}).
-			success(function(data, status, headers, config) {
-				$window.location = "/";
-			}).
-			error(function(data, status, headers, config) {
-				$log.error(data);
-				$scope.loading = false;
-				$scope.validationMessage = 'L\'identifiant ou le mot de passe que vous avez entré est incorrect.';
-			});
-	};
+    $scope.login = function (credentials) {
+        $scope.loading = true;
+        
+        $http.post($window.hostName + '/page/login/authentication', {credentials: credentials})
+        .success(function(data, status, headers, config) {
+            $window.location = "/";
+	})
+	.error(function(data, status, headers, config) {
+	    $log.error(data);
+	    $scope.loading = false;
+	    $scope.validationMessage = 'L\'identifiant ou le mot de passe que vous avez entré est incorrect.';
+	});
+    };
 }]);
